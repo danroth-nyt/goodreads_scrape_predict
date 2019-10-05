e6a2c965ade3d3d7028e73f870c74a11f2899b33
Dan Roth <rdan689@gmail.com>
2019-10-04 23:15:34 -0400

    fully working scrape code

--- goodreadscrape.py
+++ goodreadscrape.py
@@ -6,85 +6,103 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+pd.set_option('display.expand_frame_repr', False)  # display full dataframe width
 
 
 def search_gr(ref_nums):
     """Generate dataframe of Goodreads book information"""
     driver = webdriver.Chrome('/home/danroth/chromedriver')
-    # driver.set_window_position(-2000,0)  # this function will minimize the window
     ref_nums = ref_nums
-    book_nums = []
-    titles = []
-    authors = []
-    dates = []
-    ratings = []
-    genres = []
-    formats = []
-    page_num = []
-    langs = []
-    num_likes = []
+    book_nums, titles, authors, followers, dates, og_dates, ratings, genres, bindings, page_num, lang,\
+        num_likes, trivia_qs, quote_likes, rev_likes, num_ratings, num_revs = ([] for i in range(17))
     print('Searching books')
     for i, book_reference_number in enumerate(ref_nums):
         print(i, book_reference_number)
         driver.get("https://www.goodreads.com/book/show/"+str(book_reference_number))
         soup = BeautifulSoup(driver.page_source, 'lxml')
+        book_nums.append(book_reference_number)
         try:
-            book_title = soup.select('.gr-h1.gr-h1--serif')[0].text.strip()
+            titles.append(soup.select('.gr-h1.gr-h1--serif')[0].text.strip())
         except:
-            book_title = ''
+            titles.append('Unknown')
         try:
-            author_name = soup.select('.authorName')[0].text.strip()
+            authors.append(soup.select('.authorName')[0].text.strip())
         except:
-            author_name = ''
+            authors.append('Unknown')
         try:
-            rating = soup.find("span", itemprop="ratingValue").text.strip()
+            follower_str = soup.find("div", {"class": "bookAuthorProfile__followerCount"}).get_text()
+            followers.append(int(''.join(re.findall(r'\d', follower_str, re.MULTILINE))))
         except:
-            rating = ''
+            followers.append(0)
         try:
-            genre = soup.findAll("div", {"class": "elementList"})[0].a.get_text()
+            ratings.append(float(soup.find("span", itemprop="ratingValue").text.strip()))
         except:
-            genre = ''
+            ratings.append(0)
         try:
-            format = soup.find("span", itemprop="bookFormat").text.strip()
+            genres.append(soup.findAll("div", {"class": "elementList"})[0].a.get_text())
         except:
-            format = ''
+            genres.append('Unknown')
         try:
-            pages = soup.find("span", itemprop="numberOfPages").text.strip(' pages')
+            bindings.append(soup.find("span", itemprop="bookFormat").text.strip())
         except:
-            pages = ''
+            bindings.append('Unknown Binding')
+        try:
+            page_num.append(int(soup.find("span", itemprop="numberOfPages").text.strip(' pages')))
+        except:
+            page_num.append(np.nan)
         try:
             date_string = soup.findAll("div", {"class": "row"})[1].get_text()
-            date = re.search(r"(\w+\s)(\w+\s\d+$|\d+$)", date_string, re.MULTILINE).group(0)
+            dates.append(int(re.search(r"(\d{4})", date_string, re.MULTILINE).group(0)))
         except:
-            date = ''
+            dates.append('')
         try:
-            lang = soup.find("div", itemprop="inLanguage").text
+            og_date_string = soup.findAll("div", {"class": "row"})[1].get_text()
+            og_dates.append(int(re.findall(r"(\d{4})", og_date_string, re.MULTILINE)[1]))
         except:
-            lang = ''
+            og_dates.append('')
         try:
-            rating_more = driver.find_element_by_xpath("//a[@id, 'rating_details']")
-            rating_more.click()
-            likes = soup.find("span", {"class": "value"})
+            lang.append(soup.find("div", itemprop="inLanguage").text)
         except:
-            likes = ''
-        book_nums.append(book_reference_number)
-        titles.append(book_title)
-        authors.append(author_name)
-        dates.append(date)
-        ratings.append(rating)
-        genres.append(genre)
-        formats.append(format)
-        page_num.append(pages)
-        langs.append(lang)
-        num_likes.append(likes)
-        d = {'book_num': book_nums, 'title': titles, 'author': authors, 'publish_date': dates, 'rating': ratings,
-             'genre': genres, 'format': formats, 'pages': page_num, 'language': langs, 'likes': num_likes}
-        book_df = pd.DataFrame(d)
+            lang.append('Unknown')
+        try:
+            num_likes.append(int(re.findall(r'<span class=\\\"value\\\">(.*)<\\/span>% of people liked it',
+                                            str(soup), re.MULTILINE)[0]))
+        except:
+            num_likes.append(0)
+        try:
+            trivia_qs.append(int(re.search(r'\d+', soup.findAll("div", {"class": "mediumText"})[1].get_text()).group()))
+        except:
+            trivia_qs.append(0)
+        try:
+            quote_likes.append(int(re.search(r'\d+',
+                                             soup.findAll("a", {"class": "actionLinkLite"})[-2].get_text()).group()))
+        except:
+            quote_likes.append(0)
+        try:
+            rev_likes.append(int(re.search(r'\d+', soup.select('a[id*="like_count_review"]')[0].get_text()).group()))
+        except:
+            rev_likes.append(0)
+        try:
+            num_rate_str = soup.find("meta", itemprop="ratingCount")
+            num_ratings.append(int(num_rate_str["content"]))
+        except:
+            num_ratings.append(0)
+        try:
+            num_revs_str = soup.find("meta", itemprop="reviewCount")
+            num_revs.append(int(num_revs_str["content"]))
+        except:
+            num_revs.append(0)
         # if i + 1 % 150 == 0:
         #     time.sleep(320)
         time.sleep(2)
+    keys = ['book_num', 'title', 'author', 'followers', 'pub_date', 'og_pub_date', 'avg_rating', 'genre', 'binding',
+            'pages', 'language', 'perc_like', 'trivia', 'quote_likes', 'rev_likes', 'num_revs', 'num_ratings']
+    values = [book_nums, titles, authors, followers, dates, og_dates, ratings, genres, bindings, page_num, lang,
+              num_likes, trivia_qs, quote_likes, rev_likes, num_revs, num_ratings]
+    d = dict(zip(keys, values))
+    book_df = pd.DataFrame(d)
     driver.close()
-    print('Initial book scrape complete')
+    print('Book scrape complete')
     return book_df
 
 
@@ -104,17 +122,15 @@ def gr_stats(ref_nums):
     sign_in.click()
 
     l = []
-    print('Start scrape book stats')
+    print('Start book stats scrape')
     for i, book_reference_number in enumerate(ref_nums):
         print(i, book_reference_number)
         # if i + 1 % 150 == 0:
         #     time.sleep(320)
         time.sleep(2)
         stats_driver.get("https://www.goodreads.com/book/stats?id=" + str(book_reference_number))
+        stats_soup = BeautifulSoup(stats_driver.page_source, 'lxml')
         try:
-            show_table = stats_driver.find_element_by_xpath("//a[contains(@class, 'smallText')]")
-            show_table.click()
-            stats_soup = BeautifulSoup(stats_driver.page_source, 'lxml')
             table = stats_soup.find('div', id='dataTable')  # find and process table
             table_rows = table.find_all('tr')
             for tr in table_rows:
@@ -146,30 +162,20 @@ def process_gr(ref_nums):
 
 def main():
     """Essential variables"""
-    api_key = 'jpzuzysfQRRMex0Ovlmpg'
-    num_samples = 10
-    random.seed(115)
+    num_samples = 2500
+    random.seed(110)
     book_nums = random.sample(range(1, 8630000), num_samples)  # Last book is 8,630,000
 
     """Process and save dataframe"""
-    # book_df_raw = process_gr(book_nums)
-    # book_df_raw = book_df_raw.replace('', np.nan)
-    # with open('./data/book_data.pkl', 'wb') as savefile:
-    #     pickle.dump(book_df_raw, savefile)
+    book_df_raw = process_gr(book_nums)
+    book_df_raw = book_df_raw.replace('', np.nan)
+    with open('data/book_data.pkl', 'wb') as savefile:
+        pickle.dump(book_df_raw, savefile)
 
     """Open and manipulate dataframe"""
-    # with open("./data/book_data.pkl", 'rb') as openfile:
-    #     book_df = pickle.load(openfile)
-    # print(book_df.head(), book_df.tail())
-
-    """Test"""
-    # driver = webdriver.Chrome('/home/danroth/chromedriver')
-    # driver.get("https://www.goodreads.com/book/show/" + str(1000))
-    # rating_more = driver.find_element_by_xpath("//a[@id, 'rating_details']")
-    # rating_more.click()
-    # soup = BeautifulSoup(driver.page_source, 'lxml')
-    book_df = search_gr([1000])
-    print(book_df.likes)
+    with open("data/book_data.pkl", 'rb') as openfile:
+        book_df = pickle.load(openfile)
+    print(book_df.describe())
 
 
 if __name__ == "__main__":
